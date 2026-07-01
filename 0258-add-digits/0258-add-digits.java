8c1f27b5284f8d408995737a5866f0b1c0d3c6aa
Thiru@05 <thirupalamalai2005@gmail.com>
2026-07-01 23:32:30 +0530

    Time: 1 ms (97.94%), Space: 42.8 MB (10.05%) - LeetHub - Time: O(log(n)), Space: O(1)

---
+++ 0258-add-digits/0258-add-digits.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int addDigits(int num) {
+        while(num>9){
+            int temp=0;
+            while(num!=0){
+                temp=temp+num%10;
+                num/=10;
+            }
+            num=temp;
+        }
+        return num;
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
