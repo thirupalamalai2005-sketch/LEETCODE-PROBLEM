51a2f21b2be35cced73261ad178e82ef8401c385
Thiru@05 <thirupalamalai2005@gmail.com>
2026-07-06 23:54:07 +0530

    Time: 1 ms (33.68%), Space: 47.3 MB (7.39%) - LeetHub - Time: O(n), Space: O(1)

---
+++ 2515-shortest-distance-to-target-string-in-a-circular-array/2515-shortest-distance-to-target-string-in-a-circular-array.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int closestTarget(String[] words, String target, int startIndex) {
+       int sum=Integer.MAX_VALUE;
+       int j=words.length;
+       for(int i=0;i<j;i++){
+        if(words[i].equals(target)){
+            sum=Math.min(Math.min(Math.abs(i-startIndex),words.length-Math.abs(i-startIndex)),sum);
+        }
+       }
+        return sum>100?-1:sum;
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
