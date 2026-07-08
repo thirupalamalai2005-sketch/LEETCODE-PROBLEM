f2fc708f25fd899e1cf5c0c9633c67a174fcd1e4
Thiru@05 <thirupalamalai2005@gmail.com>
2026-07-09 00:07:41 +0530

    Time: 1 ms (99.99%), Space: 43.7 MB (99.37%) - LeetHub

--- 2144-minimum-cost-of-buying-candies-with-discount/2144-minimum-cost-of-buying-candies-with-discount.java
+++ 2144-minimum-cost-of-buying-candies-with-discount/2144-minimum-cost-of-buying-candies-with-discount.java
@@ -3,11 +3,13 @@ class Solution {
         int res[]=new int[101];
         int sum=0;
         int len=cost.length;
+        int max=0;
         for(int i=0;i<len;i++){
             res[cost[i]]++;
+            max=Math.max(max,cost[i]);
         }
         int i=2;
-        for(int j=100;j>=0;j--){
+        for(int j=max;j>=0;j--){
             if(res[j]==0) continue;
             while(res[j]>0){
                 if(i>0){
