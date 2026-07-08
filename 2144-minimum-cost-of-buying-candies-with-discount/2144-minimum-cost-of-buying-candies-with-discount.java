0c0de4de52522750a6a88d15e344ff3bd27cb669
Thiru@05 <thirupalamalai2005@gmail.com>
2026-07-09 00:02:13 +0530

    Time: 1 ms (99.99%), Space: 43.8 MB (99.37%) - LeetHub - Time: O(, Space: O(?)

---
+++ 2144-minimum-cost-of-buying-candies-with-discount/2144-minimum-cost-of-buying-candies-with-discount.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int minimumCost(int[] cost) {
+        int res[]=new int[101];
+        int sum=0;
+        for(int i=0;i<cost.length;i++){
+            res[cost[i]]++;
+        }
+        int i=2;
+        for(int j=100;j>=0;j--){
+            if(res[j]==0) continue;
+            while(res[j]>0){
+                if(i>0){
+                sum+=j;
+                i--;
+                }else{
+                  i=2;
+                }
+                res[j]--;
+            }
+        }
+        return sum;
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
