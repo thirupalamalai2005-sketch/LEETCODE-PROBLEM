840069eb5faca2b34c4c3a6fbbfdc072efa3302e
Thiru@05 <thirupalamalai2005@gmail.com>
2026-07-10 18:28:52 +0530

    Time: 0 ms (100%), Space: 42.3 MB (20.15%) - LeetHub

---
+++ 1518-water-bottles/1518-water-bottles.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int numWaterBottles(int numBottles, int numExchange) {
+        int sum=numBottles;
+        while (numBottles >= numExchange) {
+            int temp = numBottles / numExchange;   
+            int temp1 = numBottles % numExchange;    
+            sum += temp;                         
+            numBottles = temp + temp1;
+        }
+        return sum;
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
