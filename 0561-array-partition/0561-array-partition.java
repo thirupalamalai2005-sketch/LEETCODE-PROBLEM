6f767a741cb9b67a9a73710d2bca0a7339d902c2
Thiru@05 <thirupalamalai2005@gmail.com>
2026-07-11 22:16:03 +0530

    Time: 18 ms (33.69%), Space: 49.6 MB (53.85%) - LeetHub - Time: O(n log n), Space: O(1)

---
+++ 0561-array-partition/0561-array-partition.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int arrayPairSum(int[] nums) {
+        Arrays.sort(nums);
+        int sum=0;
+        for(int i=0;i<nums.length-1;i=i+2){
+            sum+=Math.min(nums[i],nums[i+1]);
+        }
+        return sum;
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
