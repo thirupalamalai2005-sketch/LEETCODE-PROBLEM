9542ca42696706f5573753f5a6a9a8ad07da2c4e
Thiru@05 <thirupalamalai2005@gmail.com>
2026-07-12 22:41:22 +0530

    Time: 1 ms (84.33%), Space: 42.5 MB (61.58%) - LeetHub - Time: O(1), Space: O(1)

---
+++ 3870-count-commas-in-range/3870-count-commas-in-range.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int countCommas(int n) {
+        return (n<1000)?0:n+1-1000;
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
