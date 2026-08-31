b76c47ec929b7eafa56e7e0a5cf74a47af76b5f9
Thiru@05 <thirupalamalai2005@gmail.com>
2026-08-31 22:43:54 +0530

    Time: 0 ms (100%), Space: 48 MB (47.18%) - LeetHub - Time: O(1), Space: O(1)

---
+++ 0049-powx-n/0049-powx-n.java
@@ -0,0 +1,9 @@
+class Solution {
+    public double myPow(double x, int n) {
+        return (double)Math.pow(x,n);
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
