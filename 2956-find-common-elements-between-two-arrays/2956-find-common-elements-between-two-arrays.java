79d77ad887857825b7cbcfd1e78438e1cfb124e2
Thiru@05 <thirupalamalai2005@gmail.com>
2026-08-31 21:21:18 +0530

    Time: 1 ms (100%), Space: 47.2 MB (87.64%) - LeetHub - Time: O(n + m), Space: O(1)

--- 2956-find-common-elements-between-two-arrays/2956-find-common-elements-between-two-arrays.java
+++ 2956-find-common-elements-between-two-arrays/2956-find-common-elements-between-two-arrays.java
@@ -2,22 +2,21 @@ class Solution {
     public int[] findIntersectionValues(int[] nums1, int[] nums2) {
         int arr[]=new int[101];
         int arr2[]=new int[101];
+        int max=0;
         for(int i:nums1){
             arr[i]++;
+            max=Math.max(i,max);
         }
         for(int j:nums2){
             arr2[j]++;
+            max=Math.max(j,max);
         }
         int co1=0;
         int co2=0;
-        for(int i:nums1){
-            if(arr2[i]!=0){
-                co1++;
-            }
-        }
-        for(int i:nums2){
-            if(arr[i]!=0){
-                co2++;
+        for(int i=0;i<=max;i++){
+            if(arr[i]!=0 && arr2[i]!=0){
+                co1+=arr[i];
+                co2+=arr2[i];
             }
         }
         return new int[]{co1,co2};
